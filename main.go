9903a2794d7da8204124c1f552ee86e03c44e181
zkynet <anon@zkynet.io>
2020-09-24 19:01:48 +0000

    adding the print my name function

--- main.go
+++ main.go
@@ -5,4 +5,9 @@ import "log"
 func main() {
 	log.Println("Hello world !")
 	log.Println("This is the new-print branch")
+	PrintMyName("Sveinn")
+}
+
+func PrintMyName(name string) {
+	log.Println(name)
 }
